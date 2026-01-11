19b0f0c2b6e40b3dbdb3200e092a49338216f8a2
Kelly Gibson <119765551+kelly-gibson@users.noreply.github.com>
2026-01-11 15:21:09 -0700

    Feat: Implemented a Global Writer (#3)
    * Add target specification for llvm
    
    * Add config.toml file and Implement basic text printing
    
    * Add config.toml: target runner for cargo build to boot into QEMU when using cargo run
    
    * Add: basic writer for VGA buffer
    
    * Implemented a basic writer that can be used as a global interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-// disabling the standard library
+// disables the standard library
 #![no_std]
-// telling the compiler to not use the normal entry point chain
+// tells the compiler to not use the normal entry point chain
 #![no_main]
+
+pub mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -11,8 +13,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// static HELLO: &[u8] = b"Welcome to focus OS";
+
 // Creating an entry point. Also tells the compiler to use the C calling convention, rather than the rust convention.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    //vga_buffer::print_something();
+    //vga_buffer::example_global_writer();
+    use core::fmt::Write;
+    use vga_buffer::with_writer;
+    with_writer(|writer| {
+        // Perform write operations using the writer
+        write!(writer, "The numbers are {} and {}", 56, 1.0 / 3.0).unwrap();
+    });
+
+    loop{}
 }
